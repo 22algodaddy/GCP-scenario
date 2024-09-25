841dded0675b5b47372b1fa7345860edab16578b
22algodaddy <105907882+22algodaddy@users.noreply.github.com>
2024-09-26 01:46:58 +0530

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,4 +7,5 @@ def hello_world():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = 8080
+    app.run(host='0.0.0.0', port=port)
