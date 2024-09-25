a3af648fca097c1a90ab9869754dc52745dce6f1
Dhruv Choubey <Choubeydhruv2014@gmail.com>
2024-09-25 23:17:08 +0530

    Added all the files

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,9 @@ provider "google" {
   project = "mythic-delight-434312-r1"
   region  = "us-west1"
 }
-resource "google_container_registry" "registry" {
-  location = "EU"
+resource "google_artifact_registry_repository" "my-repo" {
+  location      = "us-west1"
+  repository_id = "hu-devops"
+  description   = "example docker repository"
+  format        = "DOCKER"
 }
